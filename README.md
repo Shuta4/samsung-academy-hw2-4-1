89696a97696d70afa8a80dbd7b0926c61c3fc13d
Shuta4 <skirdin.nik@gmail.com>
2022-11-15 10:51:07 +0300

    Complete task

---
+++ README.md
@@ -0,0 +1,2 @@
+# Samsung Academy Homework 2-4-1
+Android app solving quadratic equation $ax^2 + bx + c = 0$.
--- app/src/main/java/ru/samsung/itschool/book/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/MainActivity.java
@@ -7,13 +7,68 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void run(View view) {
+        final EditText fieldA = (EditText) findViewById(R.id.a);
+        final EditText fieldB = (EditText) findViewById(R.id.b);
+        final EditText fieldC = (EditText) findViewById(R.id.c);
+        final TextView result = (TextView) findViewById(R.id.res);
 
+        double a, b, c;
+        try {
+            a = Double.parseDouble(fieldA.getText().toString());
+            b = Double.parseDouble(fieldB.getText().toString());
+            c = Double.parseDouble(fieldC.getText().toString());
+        } catch (Exception e) {
+            result.setText(R.string.enter_numbers);
+            return;
+        }
+
+        if (a == 0) {
+            if (b == 0) {
+                result.setText(c == 0 ? R.string.any_x : R.string.no_answers);
+            } else {
+                double x = -c / b;
+                result.setText(String.format(Locale.getDefault(), "%.1f", x));
+            }
+
+        } else if (b == 0) {
+            double x = 0;
+            if (c == 0) {
+                result.setText(String.format(Locale.getDefault(), "%.1f", x));
+            } else {
+                double div = -c / a;
+                if (div < 0) {
+                    result.setText(R.string.no_answers);
+                } else {
+                    x = Math.sqrt(div);
+                    result.setText(String.format(Locale.getDefault(), "%.1f %.1f", x, -x));
+                }
+            }
+        } else {
+            double d = b * b - 4 * a * c;
+            if (d < 0) {
+                result.setText(R.string.no_answers);
+
+            } else if (d == 0) {
+                double x = -b / (2 * a);
+                result.setText(String.format(Locale.getDefault(), "%.1f", x));
+
+            } else {
+                double x1 = (-b - Math.sqrt(d)) / (2 * a);
+                double x2 = (-b + Math.sqrt(d)) / (2 * a);
+                result.setText(String.format(Locale.getDefault(), "%.1f %.1f", x1, x2));
+
+            }
+        }
+
+    }
 }
